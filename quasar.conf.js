a9b380838a7c5a8584687f09ad64c2d4d2070d19
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 11:26:20 +0300

    Rokas

--- quasar.conf.js
+++ quasar.conf.js
@@ -18,7 +18,7 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v1.quasar.dev/quasar-cli/boot-files
     boot: [
-      
+
     ],
 
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -93,7 +93,7 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Dialog', 'Notify']
     },
 
     // animations: 'all', // --- includes all animations
--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -5,16 +5,31 @@
       separator
       >
       <q-item
-        v-for="task in tasks"
+        v-for="(task, index) in tasks"
         :key="task.title"
+        @click="task.done = !task.done"
+        :class="{'done bg-blue-1' : task.done}"
+        clickable
         v-ripple>
         <q-item-section avatar>
           <q-checkbox
             v-model="task.done"
+            class="no-pointer-events"
             color="primary" />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            @click.stop="deleteTask(index)"
+            flat
+            dense
+            round
+            color="primary"
+            icon="delete"
+            />
         </q-item-section>
       </q-item>
     </q-list>
@@ -42,6 +57,30 @@ export default {
         },
       ]
     }
+  },
+  methods: {
+    deleteTask(index) {
+
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'Would you like to delete task?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.tasks.splice(index, 1)
+        this.$q.notify('Task deleted')
+      })
+
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.done{
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
